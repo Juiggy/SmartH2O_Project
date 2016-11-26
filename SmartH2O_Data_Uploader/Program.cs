f922b20380aa02064788941db8ea3b2c4fe15026
Joel Rodrigues <2090715@my.ipleiria.pt>
2016-11-26 17:44:16 +0000

    Add project files.

---
+++ SmartH2O_Data_Uploader/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using SoftwareOrganizationSmartH2O; //lib SoftwareOrganization
+using uPLibrary.Networking.M2Mqtt; //lib para Mosquitto
+using uPLibrary.Networking.M2Mqtt.Messages; //lib para Mosquitto
+using System.Xml;
+using System.Xml.Schema;
+using System.IO;
+
+
+namespace SmartH2O_Data_Uploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+            int delay = SmartH2O_Data_Uploader.Properties.Settings.Default.sensorTime;
+                   
+            //Inicializo da DLL - vai estar a correr ate app encerrar
+            dll.Initialize(sendData, delay);
+
+            Console.ReadKey();
+        }
+        private static void sendData(string str)
+        {
+            //crio um objecto SensorData com os valores correctos
+            SensorData sensorData = new SensorData(str);
+
+            //chamo metodo que devolve os dados do objecto em XML
+            XmlDocument sensorXML = sensorData.getDataInXML();
+
+          
+            //valido o XML com o schema
+            XmlSchemaSet schema = new XmlSchemaSet();
+            
+            //como fazer isto de forma mais dinamica?
+            string schemaPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\SoftwareOrganizationSmartH2O\\XMLSensorDataMsgSchema.xsd";
+            schema.Add("", schemaPath);
+                       
+                
+
+            bool validationErrors = false;
+            sensorXML.Schemas.Add(schema);
+           
+            sensorXML.Validate((s, e) =>
+                {
+                    //Apresento mensagem de erro por o XML nao ser valido
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(e.Message);
+                    validationErrors = true;
+                    Console.ResetColor(); 
+
+                });
+                
+
+                
+                //publico o XML devidamente validado - Mosquitto
+            if (!validationErrors)
+            {
+                //nao houve erros, publico mensagem
+
+                Console.WriteLine("tudo okay");
+            }
+            
+         
+        }
+
+        
+    }
+}
---
+++ SoftwareOrganizationSmartH2O/SensorData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SoftwareOrganizationSmartH2O
+{
+    public class SensorData
+    {
+
+
+
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            //set { _type = value; }
+        }
+
+        private float _value;
+        public float Value
+        {
+            get { return _value; }
+            //set { _value = value; }
+        }
+
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            // set { _date = value; }
+        }
+
+        private Guid _id;
+        public Guid Id
+        {
+            get { return _id; }
+            //set { _id = value; }
+        }
+
+        public SensorData(string tipo, string valor)
+        {
+            this._type = tipo;
+            valor = valor.Replace(".", ",");
+            this._value = float.Parse(valor);
+            this._id = Guid.NewGuid();
+            this._date = DateTime.Now;
+        }
+
+        public SensorData(string sensorValue)
+        {
+            String[] sensorValues = sensorValue.Split(';');
+            this._type = sensorValues[1];
+            string valor = sensorValues[2];
+            valor = valor.Replace(".", ",");
+            this._value = float.Parse(valor);
+            this._id = Guid.NewGuid();
+            this._date = DateTime.Now;
+        }
+
+        public XmlDocument getDataInXML()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(dec);
+            XmlElement sensor = doc.CreateElement("sensor");
+            doc.AppendChild(sensor);
+           
+            
+
+            XmlElement tipo = doc.CreateElement("type");
+            tipo.InnerText = _type;
+            XmlElement id = doc.CreateElement("id");
+            id.InnerText = _id.ToString();
+            XmlElement date = doc.CreateElement("date");
+            date.InnerText = _date.ToString(); //terei de verificar o formato da hora?
+            XmlElement value = doc.CreateElement("value");
+            string aux = _value.ToString();
+            aux = aux.Replace(",", ".");
+            value.InnerText = aux;
+
+            sensor.AppendChild(tipo);
+            sensor.AppendChild(id);
+            sensor.AppendChild(date);
+            sensor.AppendChild(value);
+         
+
+
+            return doc;
+        }
+    }
+
+}
