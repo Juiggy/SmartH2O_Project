657f094e2a22267c09f826878e8f1620abf18383
vilhena32 <joana.vilhena.32@gmail.com>
2016-12-12 23:15:42 +0000

    //alarmData

---
+++ SoftwareOrganizationSmartH2O/AlarmData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SoftwareOrganizationSmartH2O
+{
+    class AlarmData
+    {
+        string parameter;
+        decimal value;
+        decimal value2;
+        string operation;
+        bool condition;
+
+
+
+    }
+}
